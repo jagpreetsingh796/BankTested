2ba5bfe6cdd1373739ca5252ccc37eb9bf631998
jagpreetsingh796 <jagpreet.singh796@gmail.com>
2019-06-19 12:27:06 -0400

    With event

--- app/ether.js
+++ app/ether.js
@@ -7,13 +7,28 @@ let wallet = new ethers.Wallet(pk,provider)
 let bytecode=aa.code
 let abi=aa.abiDefinition
 
-let factory = new ethers.ContractFactory(abi, bytecode, wallet);
-async function deploythecontract()
+// let factory = new ethers.ContractFactory(abi, bytecode, wallet);
+// async function deploythecontract()
+// {
+//     let contract = await factory.deploy()
+//     await contract.deployed()
+//     console.log(contract)
+// }
+
+// deploythecontract()
+
+let address='0x0037923C56f2fe27394A85516480445f013F8bDA'
+let contract = new ethers.Contract(address, abi, provider);
+contractWithSigner=contract.connect(wallet)
+// console.log(contractWithSigner)
+
+async function openAccount()
 {
-    let contract = await factory.deploy()
-    await contract.deployed()
-    console.log(contract)
+let tx= await contractWithSigner.openAccount("aaaa",25,"bbbb","cccc",{value:ethers.utils.parseEther("1.0")})
+console.log(tx)
 }
+openAccount()
+contract.on("bal",(bal)=>{
 
-deploythecontract()
-
+    console.log("The balance is",bal)
+})
